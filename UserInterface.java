bf8de09f1219e2fffd4a36341d90379efca0d49e
Dyeh310 <nheyd9292@gmail.com>
2017-10-28 21:36:09 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -8,60 +8,61 @@ public class UserInterface {
 
 	private Scanner reader;
 	private PasswordGenerator pg;
-	
-	
-	public UserInterface () {
+
+	public UserInterface() {
 		this.reader = new Scanner(System.in);
 		this.pg = new PasswordGenerator();
 	}
-	
-	
-	public void start () {
+
+	public void start() {
 		boolean running = true;
-		
-		//userinterface begins
-		while(running) {
-			
-			System.out.println("Do you need a password?");
-			String userEnter = reader.nextLine();
-			
-			//password is generated
-			if(userEnter.equalsIgnoreCase("yes") || userEnter.equalsIgnoreCase("y")) {
+
+		// userinterface begins
+		while (running) {
+			// password is generated
+				boolean check = true;
+				System.out.println("How many characters do you want your password to be?");
+				while (check) {
+
+					try {
+						int length = Integer.parseInt(reader.nextLine());
+						if (length > 0 && length <= 100) {
+							pg.setPasswordLength(length);
+							check = false;
+						} else {
+							System.out.println("Please type a number between 1-100");
+						}
+
+					} catch (Exception e) {
+						System.out.println("Please type a number between 1-100");
+					}
+
+				}
 				
-				String thePass =  pg.generateRandomChar(); 
+
+				String thePass = pg.generateRandomChar();
 				System.out.println("You password is: " + thePass);
-				
-				
-				//insert password begins
+
+				// insert password begins
 				while (running) {
 					System.out.println("Please enter the password to continue");
 					String input = reader.nextLine();
-					
+
 					if (input.equals(thePass)) {
 						System.out.println("Correct!");
 						running = false;
 					} else {
 						System.out.println("Incorrect");
 					}
-					
-				
-				//inster password ends	
+
+					// inster password ends
 				}
-				
-				
-				
-			} else {
-				System.out.println("invalid entry");
+
 			}
-			
-			
-			
-		//userinterface ends	
+
+			// userinterface ends
 		}
-		
+
 	}
-	
-	
-	
-	
-}
+
+
