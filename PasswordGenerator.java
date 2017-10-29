a0a5a9644bf9b141cbef368b560a079c606c76d8
Dyeh310 <nheyd9292@gmail.com>
2017-10-28 21:35:44 -0400

    Update PasswordGenerator.java

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,26 +1,68 @@
 package logic;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class PasswordGenerator {
 
-	private int passwordLength = 6;
+	private int passwordLength;
 	private Random random;
+	private char[] punct = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ':', '<', '>', '?' };// 14
+	private int sbLength;
+	private StringBuilder sb;
+	private char rpwChar;
+	private char rpwP;
+	private int rpwNum;
 
 	public PasswordGenerator() {
 		this.random = new Random();
+		this.sbLength = sbLength;
+		this.sb = new StringBuilder("");
+		this.rpwChar = rpwChar;
+		this.rpwP = rpwP;
+		this.rpwNum = rpwNum;
+	}
+
+	public int getPasswordLength() {
+		return passwordLength;
+	}
+
+	public void setPasswordLength(int passwordLength) {
+		this.passwordLength = passwordLength;
 	}
 
 	public String generateRandomChar() {
-		StringBuilder sb = new StringBuilder("");
-		for (int i = 0; i < passwordLength;i++) {
-			char rpwChar = (char) (random.nextInt(26)+'a');
-			int rpwNum = random.nextInt(10);
-			sb.append(rpwChar);
-			sb.append(rpwNum);
+		for (int i = 0; i < passwordLength; i++) {
 			
+			int choice = random.nextInt(3);
+			
+			rpwChar = (char) (random.nextInt(26) + 'a');
+			if (random.nextInt(2) == 1) {
+				rpwChar = Character.toUpperCase(rpwChar);
+			}
+			rpwP = punct[i];
+			rpwNum = random.nextInt(10);
+
+			if (sbLength < passwordLength && choice == 0) {
+				sb.append(rpwChar);
+				sbLength++;
+			}
+			if (sbLength < passwordLength && choice == 1) {
+				sb.append(rpwNum);
+				sbLength++;
+			}
+
+			if (sbLength < passwordLength && choice == 2) {
+				sb.append(rpwP);
+				sbLength++;
+			}
+
 		}
+
+		Collections.shuffle(Arrays.asList(sb));
 		return sb.toString();
 	}
-
 }
+
